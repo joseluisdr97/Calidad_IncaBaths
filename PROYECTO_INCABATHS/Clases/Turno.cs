340798d05d6efe21aff10af7cacf26d0e4a26113
JOSE LUIS <joseluisdr97@gmail.com>
2020-05-06 17:56:03 -0500

    Esta es la version inicial del proyecto

---
+++ PROYECTO_INCABATHS/Clases/Turno.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PROYECTO_INCABATHS.Clases
+{
+    public class Turno
+    {
+        public int IdTurno { get; set; }
+        public int IdServicio { get; set; }
+        public TimeSpan HoraInicio { get; set; }
+        public TimeSpan HoraFin { get; set; }
+        public DateTime Fecha { get; set; }
+
+        //Relaciones
+        public Servicio Servicio { get; set; }
+        public List<DetalleReserva> DetalleReservas { get; set; }
+    }
+}
